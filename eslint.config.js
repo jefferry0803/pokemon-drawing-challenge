cfb35e350a916e30fd785fc24a2e0f24da340524
Jefferry Chiu <jefferry0803@yahoo.com.tw>
2025-06-11 17:43:04 +0800

    feat: 讓 Eslint 支援 Typescript

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,26 @@ import js from '@eslint/js';
 import eslintConfigPrettier from '@vue/eslint-config-prettier';
 import pluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: { '@typescript-eslint': tseslint },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
   {
     languageOptions: {
       sourceType: 'module',
@@ -16,6 +32,7 @@ export default [
     },
     rules: {
       'prettier/prettier': 'warn',
+      'no-console': 'warn',
     },
   },
   eslintConfigPrettier,
